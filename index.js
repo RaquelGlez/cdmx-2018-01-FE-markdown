4b7a6849c16c44810d59fb2848c84d10f55d7bf4
RaquelGlez <gonzalez.c.raquel@gmail.com>
2018-09-17 09:59:28 -0500

    refactorizacion de funciones y linea de comando

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
 // Para que nuestro archivo sea ejecutable por node
+
+const mdLinks = require('./md-links')
--- md-links.js
+++ md-links.js
@@ -9,19 +9,20 @@ const fetch = require('node-fetch');
 const file = 'README.md'
 
 const definePathAbs = (bypath) => {
-
-    return new Promise((resolve, reject) => {
-        if(!path.isAbsolute(bypath)){
-            let pathAbs = path.resolve(bypath)
-            resolve(pathAbs)
-            console.log(pathAbs)
+    return new Promise((resolve, reject)=>{
+        if(!bypath){
+            return reject(`No se tiene la ruta a ${bypath}`)
         } else {
-            reject('No tienes README.md')
+            if(!path.isAbsolute(bypath)){
+                let pathAbs = path.resolve(bypath)
+                // console.log(pathAbs)
+                return resolve(pathAbs)
+            } 
         }
     })
-};
-
+}
 //definePathAbs(file)
+
 const getUrl = (pathAbs) => {
     return new Promise((resolve, reject) =>{
         fs.readFile(pathAbs,'utf8',(err,data) =>{
@@ -29,22 +30,26 @@ const getUrl = (pathAbs) => {
                 return reject(err)
             } else{
                 let text = data;
-                console.log(data);
+                //console.log(data);
                 let reExpUrl = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&/\/=]*)/gim;
-                let reExprDescription = /\[([^\]]*)\]/gim;
+                let reExprDescription = /\[((?!{)[^\]]*)\]/gim;
                 let urls = text.match(reExpUrl)
                 let descriptions= text.match(reExprDescription)
-                console.log(urls)
-                console.log(descriptions)
-                resolve(urls,descriptions)
+                //console.log(urls)
+                //console.log(descriptions)
+                let results = {
+                    urls, descriptions
+                }
+                console.log(results)
+                resolve(results);
             }
-
         })
     })
 }
 
-const verifyUrl = (urls) => {
+const verifyUrl = (results) => {
     return new Promise((resolve, reject) => {
+        let urls = results.urls
         if(urls){
             for (let i=0; i < urls.length; i++){
                 let url = urls[i];
@@ -52,9 +57,9 @@ const verifyUrl = (urls) => {
                 fetch(url).then(res =>{
                     //console.log(res.status)
                     if(res.status == 404){
-                        urlStatus = 'fail 404';
+                        urlStatus = `${url} fail 404`;
                     } else {
-                        urlStatus = 'ok 200';
+                        urlStatus = `${url} ok 200`;
                     }
                     resolve(urlStatus)
                     console.log(urlStatus)
